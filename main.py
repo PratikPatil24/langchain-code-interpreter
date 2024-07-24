bca1c21ae9dadd34ed50c01c719836de95944482
pratik-patil <patilgpratik@gmail.com>
2024-07-24 10:04:18 +0530

    fix: CSV Agent package fixes

--- main.py
+++ main.py
@@ -21,41 +21,41 @@ def main():
     If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
     """
 
-    # base_prompt = hub.pull("langchain-ai/react-agent-template")
-    # prompt = base_prompt.partial(instructions=instructions)
-    base_prompt_template = """
-    {instructions}
-
-    TOOLS:
-    ------
-
-    You have access to the following tools:
-
-    {tools}
-
-    To use a tool, please use the following format:
-
-    ```
-    Thought: Do I need to use a tool? Yes
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ```
-
-    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-
-    ```
-    Thought: Do I need to use a tool? No
-    Final Answer: [your response here]
-    ```
-
-    Begin!
-
-    New input: {input}
-    {agent_scratchpad}"""
-    prompt = PromptTemplate(template=base_prompt_template).partial(
-        instructions=instructions
-    )
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+    # base_prompt_template = """
+    # {instructions}
+    #
+    # TOOLS:
+    # ------
+    #
+    # You have access to the following tools:
+    #
+    # {tools}
+    #
+    # To use a tool, please use the following format:
+    #
+    # ```
+    # Thought: Do I need to use a tool? Yes
+    # Action: the action to take, should be one of [{tool_names}]
+    # Action Input: the input to the action
+    # Observation: the result of the action
+    # ```
+    #
+    # When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+    #
+    # ```
+    # Thought: Do I need to use a tool? No
+    # Final Answer: [your response here]
+    # ```
+    #
+    # Begin!
+    #
+    # New input: {input}
+    # {agent_scratchpad}"""
+    # prompt = PromptTemplate(template=base_prompt_template).partial(
+    #     instructions=instructions
+    # )
     print(prompt)
 
     tools = [PythonREPLTool()]
@@ -77,6 +77,7 @@ def main():
         llm=llm,
         path="episode_info.csv",
         verbose=True,
+        allow_dangerous_code=True,
     )
 
     csv_agent.invoke(
